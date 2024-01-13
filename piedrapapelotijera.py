ba0b325cb6a12c577552e9cefbdc9b093efd5b11
= <=>
2024-01-13 23:47:15 +0100

    Segunda version - decimos lo que saca la máquina

--- piedrapapelotijera.py
+++ piedrapapelotijera.py
@@ -7,5 +7,10 @@ tirada = input("escoge tu tirada: ")
 
 tiradaordenador = random.randint(1,3)
 
-print(tirada)
-print(tiradaordenador)
+if tiradaordenador == 1:
+    print("el ordenador ha sacado piedra")
+elif tiradaordenador == 2:
+    print("el ordenador ha sacado papel")
+elif tiradaordenador == 3:
+    print("el ordenador ha sacado tijera")
+
